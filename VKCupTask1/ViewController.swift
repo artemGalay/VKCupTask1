568173e857e5b5721925733b15e50a7f6e34fc70
Artem Galay <iostema@mail.ru>
2022-12-05 18:30:59 +0300

    Initial Commit

---
+++ VKCupTask1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKCupTask1
+//
+//  Created by Артем Галай on 5.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
