49c27f90675718c4f89d8e3e09894f5fe5ad389b
Artem Galay <iostema@mail.ru>
2022-12-06 14:47:51 +0300

    Create titleLabel and skipButton and collectionView.

--- VKCupTask1/ViewController.swift
+++ VKCupTask1/ViewController.swift
@@ -9,8 +9,89 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var selected = [String]()
+    private var titles = ["Юмор", "Еда", "Кино", "Рестораны", "Прогулки", "Политика", "Новости", "Автомобили", "Сериалы", "Рецепты", "Работа", "Отдых", "Спорт", "Политика", "Новости", "Юмор", "Еда", "Кино", "Рестораны", "Прогулки", "Политика", "Новости", "Юмор", "Еда", "Кино"  ]
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.48)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "SFPro-Regular", size: 16)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        var paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 1.05
+        label.attributedText = NSMutableAttributedString(string: "Отметьте то, что вам интересно, чтобы настроить Дзен",
+                                                         attributes: [NSAttributedString.Key.kern: -0.31,
+                                                                      NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        return label
+    }()
+
+    private lazy var skipButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Позже", for: .normal)
+        button.setTitleColor(UIColor(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        button.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.12).cgColor
+        button.layer.cornerRadius = 20
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = TagFlowLayout()
+        layout.estimatedItemSize = CGSize(width: 140, height: 40)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupHierarhy()
+        setuoLayout()
     }
+
+    private func setupHierarhy() {
+        view.addSubview(titleLabel)
+        view.addSubview(skipButton)
+        view.addSubview(collectionView)
+
+    }
+
+    private func setuoLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.heightAnchor.constraint(equalToConstant: 43),
+            titleLabel.widthAnchor.constraint(equalToConstant: 252),
+
+            skipButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            skipButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            skipButton.heightAnchor.constraint(equalToConstant: 43),
+            skipButton.widthAnchor.constraint(equalToConstant: 79),
+
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        titles.count
+    }
+
+
 }
